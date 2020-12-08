4e9b3bd51fc64f304ac606dd134fa5bc91c41e21
KarLanuza <KarLanuza@gmail.com>
2020-12-09 01:52:59 +0800

    Added New Button for New Author in Author's page

--- project/resources/views/authors/index.blade.php
+++ project/resources/views/authors/index.blade.php
@@ -6,20 +6,14 @@
 <div class="content">
     <!-- Main content -->
     <section class="content">
-      
       <div class="row">
         <div class="col-xs-12">
           <div class="box">
-            
-
-            
+            <div class="box-header with-border">
+                <button href="#addNewAuthor" data-toggle="modal" class="btn btn-dark btn-outline-secondary btn-sm btn-flat"><i class="fa fa-plus"></i> New</button>
+            </div>
             <div class="box-body">
-
-
-            
               <table id="example1" class="table table-hover table-bordered">
-
-              
                 <thead>
                   <th>#</th>
                   <th>Initials</th>
@@ -27,16 +21,14 @@
                   <th>Origin of Country</th>
                 </thead>
                 <tbody>
-
-                @foreach($author as $auth)
-                 <tr>
-                   <td>{{ $auth->id }}</td>
-                    <td><a href="/authors/{{ $auth->id }}">{{ $auth->initials }}</a></td>
-                    <td>{{ $auth->age }}</td>
-                    <td>{{ $auth->country }}</td>
-                 </tr>
-                 @endforeach
-               
+                  @foreach($author as $auth)
+                  <tr>
+                    <td>{{ $auth->id }}</td>
+                      <td><a href="/authors/{{ $auth->id }}">{{ $auth->initials }}</a></td>
+                      <td>{{ $auth->age }}</td>
+                      <td>{{ $auth->country }}</td>
+                  </tr>
+                  @endforeach
                 </tbody>
               </table>
 
@@ -47,8 +39,6 @@
         </div>
       </div>
       @include('authors.modals')
-    </section>   
-    
-    
+    </section>      
   </div>
 @endsection
