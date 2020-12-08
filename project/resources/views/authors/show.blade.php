af943d6af25c5aedab24057a8be786c96c4eaba1
KarLanuza <KarLanuza@gmail.com>
2020-12-09 02:03:31 +0800

    Added Cancel button and changed UI of Author details form

--- project/resources/views/authors/show.blade.php
+++ project/resources/views/authors/show.blade.php
@@ -3,26 +3,19 @@
 
 
 
-<div class="content-wrapper">
+<div class="content-fluid">
   <!-- Content Header (Page header) -->
   <section class="content-header">
     <h1>
       Authors
     </h1>
-    <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-      <li class="active">Authors</li>
-    </ol>
   </section>
   <!-- Main content -->
   <section class="content">
-
     <div class="row">
       <div class="col-xs-12">
         <div class="box">
           <div class="box-body">
-
-
             <form method="POST" action="">
               <!-- <div class="form-group">
                   <label for="inputISBN">First Name</label>
@@ -32,7 +25,6 @@
                   <label for="inputTitle">Last Name</label>
                   <input type="text" class="form-control" id="lastname" name="lastname" value="{{ $authors->lastname }}" placeholder="Last Name">
                 </div>
-
                 <div class="form-group">
                   <label for="inputTitle">Initials</label>
                   <input type="text" class="form-control" id="initials" name="initials" value="{{ $authors->initials }}" placeholder="Initials">
@@ -41,24 +33,17 @@
                   <label for="inputTitle">Age</label>
                   <input type="text" class="form-control" id="age" name="age" value="{{ $authors->age }}" placeholder="Age">
                 </div>
-
                 <div class="form-group">
                   <label for="inputTitle">Country</label>
                   <input type="text" class="form-control" id="country" name="country" value="{{ $authors->country }}" placeholder="Country">
                 </div>
-
-
-
                 @csrf
 
                 <div class="modal-footer">
-
+                  <a class="btn btn-default btn-flat pull-left" href="/authors">Cancel</a>
                   <button type="submit" class="btn btn-primary btn-flat" name="add"><i class="fa fa-save"></i> Save</button>
-
                 </div>
               </form>
-
-
               <div class="form-group">
                 <label for="inputISBN">List of Books Published</label>
                 <ul>
