38e69e6c42f7ef168a20a331f50e435e06ed79f0
KarLanuza <KarLanuza@gmail.com>
2020-12-10 08:34:17 +0800

    changed view in authors table and improved pagination

--- project/resources/views/authors/index.blade.php
+++ project/resources/views/authors/index.blade.php
@@ -12,33 +12,35 @@
             <div class="box-header with-border">
                 <button href="#addNewAuthor" data-toggle="modal" class="btn btn-dark btn-outline-secondary btn-sm btn-flat"><i class="fa fa-plus"></i> New</button>
             </div>
-            <div class="box-body">
-              <table id="example1" class="table table-hover table-bordered">
+            <div class="tableFixHead">
+              <table class=" table-hover table-bordered">
                 <thead>
-                  <th>#</th>
-                  <th>Initials</th>
-                  <th>Age</th>
-                  <th>Origin of Country</th>
+                  <tr>
+                    <th>#</th>
+                    <th>Initials</th>
+                    <th>Age</th>
+                    <th>Origin of Country</th>
+                  </tr>
                 </thead>
                 <tbody>
                   @foreach($author as $auth)
-                  <tr>
-                    <td>{{ $auth->id }}</td>
-                      <td><a href="/authors/{{ $auth->id }}">{{ $auth->initials }}</a></td>
-                      <td>{{ $auth->age }}</td>
-                      <td>{{ $auth->country }}</td>
-                  </tr>
+                    <tr>
+                      <td>{{ $auth->id }}</td>
+                        <td><a href="/authors/{{ $auth->id }}">{{ $auth->initials }}</a></td>
+                        <td>{{ $auth->age }}</td>
+                        <td>{{ $auth->country }}</td>
+                    </tr>
                   @endforeach
                 </tbody>
               </table>
-
-              {{ $author->links()}}
-         
             </div>
+
+            
           </div>
         </div>
       </div>
-      @include('authors.modals')
-    </section>      
-  </div>
+    @include('authors.modals')
+  </section>
+  {{ $author->links('pagination::bootstrap-4')}}      
+</div>
 @endsection
