1a8e030ccc6d38e89e90cbbba477cbc2881595e4
KarLanuza <KarLanuza@gmail.com>
2020-12-11 00:44:49 +0800

    added backend code for getting countries of origin, average age and books of author and total number of authors

--- project/app/Http/Controllers/AuthorsController.php
+++ project/app/Http/Controllers/AuthorsController.php
@@ -8,61 +8,64 @@ use Illuminate\Http\Request;
 
 class AuthorsController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $author = Author::orderBy('initials', 'ASC')->paginate(50);
         $authors = Author::all();
+        $books = Book::all();
 
-        $countBooks = Book::distinct('title')->count('title');
+        $averageAge = Author::avg('age');
+        $countries = Author::all('country')->unique('country');
+        $totalCountries = count($countries);
+
+        $allAuthors = count($authors);
+        $allBooks = count($books);
+
+        $booksPerAuthor = $allBooks/$allAuthors;
 
         return view('authors.index', [
-            'authors' => $authors,
+            'allAuthors' => $allAuthors,
             'author' => $author,
-            'countBooks' => $countBooks
+            'averageAge' => $averageAge,
+            'booksPerAuthor' => $booksPerAuthor,
+            'totalCountries' => $totalCountries,
         ]);
     }
-
     
-    public function show($id){
-        
+    public function show($id)
+    {    
         $authors = Author::find($id);
         $booksOfAuthor =  Book::where('authors_id', $id)->get();
         
         return view('authors.show', [
             'authors' => $authors,
-            'booksOfAuthor' => $booksOfAuthor,
-            
+            'booksOfAuthor' => $booksOfAuthor,          
         ]);
     }
 
-    public function update($id){
-
+    public function update($id)
+    {
         $author = Author::find($id);
 
-        // $author->firstname = request('firstname');
         $author->lastname = request('lastname');
         $author->initials = request('initials');
         $author->age = request('age');
         $author->country = request('country');
         $author->save();
 
-
-         return redirect('/authors')->with('success', 'Author has been updated successfully');
+        return redirect('/authors')->with('success', 'Author has been updated successfully');
     }
     
-
-
-    public function store(){
-
+    public function store()
+    {
         $author = new Author();
 
-        // $author->firstname = request('firstname');
         $author->lastname = request('lastname');
         $author->initials = request('initials');
         $author->age = request('age');
         $author->country = request('country');
         $author->save();
 
-
-         return redirect('/authors')->with('success', 'Author has been added successfully');
+        return redirect('/authors')->with('success', 'Author has been added successfully');
     }
 }
