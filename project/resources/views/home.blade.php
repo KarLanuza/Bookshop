74cba2119ccfa57db944ad5fe1f446c135b73889
KarLanuza <KarLanuza@gmail.com>
2020-12-17 02:33:23 +0800

    modified homepage

--- project/resources/views/home.blade.php
+++ project/resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('includes.layout')
 @section('content')
-    <div class="content">
+    <div class="content col-12">
         <div class="jumbotron text-center">
             <h1>Welcome to our Bookshop!</h1> 
             <p>Please feel free to browse</p>
