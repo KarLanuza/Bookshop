fbfc608dfeadc24d7fe58513a2890b2b456ca2d9
KarLanuza <KarLanuza@gmail.com>
2020-12-16 09:12:37 +0800

    modified naming convention in schema

--- project/database/migrations/2020_11_30_205605_create_books_table.php
+++ project/database/migrations/2020_11_30_205605_create_books_table.php
@@ -15,18 +15,16 @@ class CreateBooksTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('authors_id');
+            $table->unsignedBigInteger('author_id');
             $table->string('isbn');
             $table->string('title');
             $table->integer('pages');
             $table->timestamps();
-
-            // $table->foreign('authors_id')->references('id')->on('authors');
         });
 
 
         Schema::table('books', function($table) {
-            $table->foreign('authors_id')->references('id')->on('authors')->onDelete('cascade');
+            $table->foreign('author_id')->references('id')->on('authors')->onDelete('cascade');
        });
     }
 
