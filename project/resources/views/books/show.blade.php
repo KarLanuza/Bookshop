4b7b4848e7e72b0bcb596278d715eb1d54c542d5
KarLanuza <KarLanuza@gmail.com>
2020-12-17 01:15:23 +0800

    added error message and validation in view for author details

--- project/resources/views/books/show.blade.php
+++ project/resources/views/books/show.blade.php
@@ -4,71 +4,63 @@
 
 
 <div class="container-fluid">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <h1>
-      Book
-    </h1>
-  </section>
-  <!-- Main content -->
   <section class="content">
-
     <div class="row">
-      <div class="col-xs-12">
-        <div class="box">
-          <div class="box-body">
-
-
+      <div class="col-md-6 offset-md-3">
+        <div class="card card-outline-secondary">
+          <div class="card-header">
+            <h3 class="mb-0">Book Details</h3>
+          </div>
+          <div class="card-body">
             <form method="POST" action="">
               <div class="form-group">
                 <label for="inputISBN">ISBN #</label>
-                <input type="text" class="form-control" id="isbn" name="isbn" value="{{ $books->isbn }}" placeholder="ISBN">
+                <input type="text" class="form-control @error('isbn') is-invalid @enderror" id="isbn" name="isbn" value="{{ $books->isbn }}" placeholder="ISBN">
+                @error('isbn')
+                    <p class="text-danger">{{ $errors->first('isbn') }}</p>
+                @enderror
               </div>
               <div class="form-group">
                 <label for="inputTitle">Title</label>
-                <input type="text" class="form-control" id="title" name="title" value="{{ $books->title }}" placeholder="Title">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ $books->title }}" placeholder="Title">
+                @error('title')
+                    <p class="text-danger">{{ $errors->first('title') }}</p>
+                @enderror
               </div>
-              <!-- <div class="form-group">
-                  <label for="inputAuthor">Author</label>
-                  <input type="text" class="form-control" id="author" value="{{ $books->authors_id }}" placeholder="Author">
-                </div> -->
-                <div class="form-group">
-                  <label>Author</label>
-                  <select class="form-control"  name="authors_id" id="authors_id">
-                    <option>--Select Author--</option>
-                    @foreach($authors as $author)
-                    <option value="{{ $author->id }}" 
-                      @if($author->id === $books->author->id)
-                      selected
-                      @endif
-                      >{{ $author->initials }} </option>
-                      @endforeach
-                    </select>
-                  </div>
-                  <div class="form-group">
-                    <label for="inputPages">Number of Pages</label>
-                    <input type="text" class="form-control" id="pages" name="pages" value="{{ $books->pages }}" placeholder="No of Pages">
-                  </div>
+              <div class="form-group">
+                <label>Author</label>
+                <select class="form-control"  name="authors_id" id="authors_id">
+                  <option>--Select Author--</option>
+                  @foreach($authors as $author)
+                  <option value="{{ $author->id }}" 
+                    @if($author->id === $books->author->id)
+                    selected
+                    @endif
+                    >{{ $author->initials }} </option>
+                    @endforeach
+                </select>
+                @error('author_id')
+                    <p class="text-danger">{{ $errors->first('author_id') }}</p>
+                @enderror
+              </div>
+              <div class="form-group">
+                <label for="inputPages">Number of Pages</label>
+                <input type="text" class="form-control" id="pages" name="pages" value="{{ $books->pages }}" placeholder="No of Pages">
+              </div>
 
-                  @csrf
+              @csrf
 
-                  <div class="modal-footer">
-                    <a class="btn btn-default btn-flat pull-left" href="/books">Cancel</a>
-                    <button type="submit" class="btn btn-primary btn-flat" name="add"><i class="fa fa-save"></i> Save</button>
-
-                  </div>
-                </form>
-
-
-                
-
+              <div class="modal-footer">
+                <a class="btn btn-default btn-light btn-flat pull-left" href="/books">Cancel</a>
+                <button type="submit" class="btn btn-primary btn-flat" name="add"><i class="fa fa-save"></i> Save</button>
 
               </div>
-            </div>
+            </form>
           </div>
         </div>
-      </section>   
-
-
+      </div>
     </div>
+    
+  </section>   
+</div>
     @endsection
